f6ef68399cbcde4082dd90e44767e21069a75a50
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-12-02 11:52:51 +0100

    importing THREE as a Module

--- webpack/src/script.js
+++ webpack/src/script.js
@@ -1,1 +1,29 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "#ff000" });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const sizes = {
+	width: 800,
+	height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(72, sizes.width / sizes.height);
+camera.position.z = 2;
+scene.add(camera);
+
+const canvas = document.querySelector(".webgl");
+
+const renderer = new THREE.WebGLRenderer({
+	canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
